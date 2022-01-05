40931f2e8bc8f984958d609f308d92d4f8159198
Ulises Lopez - A838598 <victor-ulises.lopez@atos.net>
2022-01-05 15:55:15 -0600

    Course viode 8 complete

---
+++ Javascript OOP/script/CallingFunctions.js
@@ -0,0 +1,17 @@
+// Method #1 (In the global object)
+function foo() {
+    console.log('Hello!');
+}
+foo();
+
+// Method #2 (In a specific object)
+let obj = {};
+obj.foo = function() {
+    console.log('Hello!');
+};
+obj.foo();
+
+// Method #3 (In an implicit created object)
+new foo();
+
+// Method #4
