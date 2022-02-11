7be664a96bceab3464df4b925998c9c91fe9756b
Ulises Lopez - A838598 <victor-ulises.lopez@atos.net>
2022-02-11 16:41:53 -0600

    Started chapter 05

---
+++ 05-hook-app/src/index.js
@@ -0,0 +1,10 @@
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import { HooksApp } from './HooksApp';
+
+ReactDOM.render(
+  <StrictMode>
+    <HooksApp />
+  </StrictMode>,
+  document.getElementById('root')
+);
