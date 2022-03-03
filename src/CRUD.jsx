5f8f55d60997100e7b1ca88709c7e2652c2c6744
Ulises Lopez - A838598 <victor-ulises.lopez@atos.net>
2022-03-03 17:30:42 -0600

    CRUD skeleton created

---
+++ src/CRUD.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import './style/css/main.css'
+
+const CRUD = props => {
+  return (
+    <div>CRUD</div>
+  )
+}
+
+CRUD.propTypes = {
+  props: PropTypes.object,
+}
+
+export default CRUD
+skeleton 
